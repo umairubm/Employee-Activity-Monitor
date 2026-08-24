66d8f6210ab294d3a1f8ab01d895cd300756cfca
hassanrazaubm-sys <hassanraza.ubm@gmail.com>
2026-08-24 14:53:05 +0500

    chore: bump version to 1.1.37

--- agent/agent.py
+++ agent/agent.py
@@ -45,7 +45,7 @@ else:
     from . import system_info as system_info_mod
 
 # You can change this to 1.1.32, etc. to test auto-update
-AGENT_VERSION = "1.1.36"
+AGENT_VERSION = "1.1.37"
 POLL_SECONDS = 15
 
 def _now_iso() -> str:
