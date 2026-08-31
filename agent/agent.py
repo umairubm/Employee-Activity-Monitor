30dcaca87d72eef73ec047d454412e9db76a63ac
UBM Technologies <ubmtechnologies@UBMs-MacBook-Pro.local>
2026-08-31 13:39:04 +0500

    chore: bump agent version to 1.1.61 for new release build

--- agent/agent.py
+++ agent/agent.py
@@ -45,7 +45,7 @@ else:
     from . import system_info as system_info_mod
 
 # You can change this to 1.1.32, etc. to test auto-update
-AGENT_VERSION = "1.1.60"
+AGENT_VERSION = "1.1.61"
 POLL_SECONDS = 15
 
 def _now_iso() -> str:
